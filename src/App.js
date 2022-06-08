15867048a56eb736afef6446b74448e3a924919f
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-08 21:32:47 +0530

    Added : Menubar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Menubar from './Components/Menubar';
 import AppRouter from './Routes/index'
 
 function App() {
   return (
   <BrowserRouter>
+  <Container>
+    <Menubar/>
     <AppRouter/>
+
+  </Container>
   </BrowserRouter>
   );
 }
---
+++ src/Components/Menubar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const Menubar = () => {
+  function handleItemClick() {
+    return "null";
+  }
+
+  return (
+    <Menu stackable>
+      <Link to="/home">
+        <Menu.Item name="home" onClick={handleItemClick}>
+          Home
+        </Menu.Item>
+      </Link>
+
+      <Link to="/student">
+        <Menu.Item name="ipfs" onClick={handleItemClick}>
+          Student
+        </Menu.Item>
+      </Link>
+    </Menu>
+  );
+};
+
+export default Menubar;
