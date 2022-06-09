a5cf32bf029d33905cc16f291754c339c1ac38ca
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-09 23:20:28 +0530

    Added: Form with name, regNo and batch input fields

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form, Input } from 'semantic-ui-react'
 
 const Student = () => {
-  return (
-    <div>Student</div>
+  const [batch, setBatch] = useState('')
+
+  return(
+  <Form>
+    <Form.Field>
+      <label>Name</label>
+      <input placeholder='Name with initial' />
+    </Form.Field>
+    <Form.Field>
+      <label>Batch</label>
+      <input placeholder='Batch' onChange={(e)=> setBatch(e.target.value)}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Registration Number</label>
+      <Input label={`EG/20${batch-2}/`} placeholder='Last 4 digits' />
+    </Form.Field>
+   
+    <Button type='submit'>Submit</Button>
+  </Form>
   )
 }
 
 export default Student
+
+
