fb37d6bb2bfb26191a81de2c65b9309005395279
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-11 22:59:02 +0530

    Added: Delete Hanler to delete the data

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form, Input, Table , Divider} from "semantic-ui-react";
+import axios from "axios";
 
 const Student = () => {
   const [batch, setBatch] = useState("");
@@ -12,18 +13,25 @@ const Student = () => {
       console.log(response.data);
       setStudents(response.data);
     });
-  }, [student]);
+  }, [students]);
   
   const submitHandler=(event)=>{
     event.preventDefault();
     const data = {
       name: name,
       regNo: reg,
-      batch:  partseInt(batch)
+      batch:  parseInt(batch)
     }
 
     axios.post("https://localhost:5001/student", data).then((res) => {
       console.log(res.data);
+    });
+  }
+
+  const deleteHandler=(id)=>{
+    axios.delete(`https://localhost:5001/api/student/${id}`)
+    .then((response) => {
+      console.log(response.data);
     });
   }
 
@@ -34,18 +42,19 @@ const Student = () => {
       <Form onSubmit={submitHandler}>
         <Form.Field>
           <label>Name</label>
-          <input placeholder="Name with initial" onChange={(e)=> setName(e.target.value)} />
+          <input name="name" placeholder="Name with initial" onChange={(e)=> setName(e.target.value)} />
         </Form.Field>
         <Form.Field>
           <label>Batch</label>
           <input
+            name="batch"
             placeholder="Batch"
             onChange={(e) => setBatch(e.target.value)}
           />
         </Form.Field>
         <Form.Field>
           <label>Registration Number</label>
-          <Input label={`EG/20${batch - 2}/`} placeholder="Last 4 digits" onChange={(e)=>setReg(e.target.value)} />
+          <Input name="reg" label={`EG/20${batch - 2}/`} placeholder="Last 4 digits" onChange={(e)=>setReg(e.target.value)} />
         </Form.Field>
 
         <Button type="submit">Submit</Button>
@@ -60,6 +69,7 @@ const Student = () => {
             <Table.HeaderCell>Name</Table.HeaderCell>
             <Table.HeaderCell>Registration Number</Table.HeaderCell>
             <Table.HeaderCell>Batch</Table.HeaderCell>
+            <Table.HeaderCell>Delete</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -69,6 +79,9 @@ const Student = () => {
             <Table.Cell>Nufail M.F.M</Table.Cell>
             <Table.Cell>EG/2017/3254</Table.Cell>
             <Table.Cell>19</Table.Cell>
+            <Table.Cell>
+            <Button onClick={deleteHandler} negative>Delete</Button>
+            </Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
